b8dcde48e6db9ca58c0914c98d6e3a9df20798da
Piyush Dawkahre <155132306+INDJokerGamma@users.noreply.github.com>
2024-11-27 21:07:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 /*
 If you want to know how this game was made, check out this video, that explains how it's made: 
 
-https://youtu.be/eue3UdFvwPo
-
-Follow him on twitter for more: https://twitter.com/HunorBorbely
-
-author : Hunor Marton Borbely 
-
 */
 
 // Extend the base functionality of JavaScript
